909525ffce7a5ab108c1ffc05fc69c36fdf7f816
Scott Schulte <scott.r.schulte@gmail.com>
2024-04-14 16:18:50 -0500

    Final changes.
    Make sure code is neat.
    Add line 58 to test removeItem further.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -8,7 +8,7 @@ const maxItems = 5;
 function addItem(item) {
   if (basket.length < maxItems) {
     basket.push(item);
-    console.log('added', item);
+    console.log('Added', item);
     return true;
   } else if (isFull() === true) {
     console.log('Cannot add item. Cart is full.');
@@ -40,29 +40,30 @@ function removeItem(item) {
   let elementPosition = basket.indexOf(item);
   if (elementPosition !== -1) {
     basket.splice(elementPosition, 1);
-    console.log(`Removed ${item}.`);
+    console.log(`Removed ${item}`);
     return item;
   } else if(elementPosition === -1) {
     return null;
   }
 }
 
-
 addItem('eggs');
 addItem('onions');
 addItem('spinach');
 removeItem('spinach');
+removeItem('onions')
 addItem('tomatoes');
 addItem('cheddar cheese');
 addItem('ham');
 addItem('milk');
 
-// empty(basket);
-// listItems();
-// console.log(isFull());
+listItems();
+console.log(isFull());
 console.log(`Basket is ${basket}`);
 console.log(basket.length);
-// console.log(`Basket is now ${basket}`);
+empty(basket);
+console.log(basket.length);
+console.log(basket);
 
 
 
